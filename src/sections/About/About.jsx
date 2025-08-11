a0af250c7effa1fa8e8dc3d197a350c23128a8be
Gian <gianlucavanella99@gmail.com>
2025-08-11 15:25:06 -0300

    About 1080

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -45,6 +45,11 @@ function About() {
                     </div>
                 </div>
             </div>
+            <div className='detalle-1080'>
+                <div className="paralelogramo4"></div>
+                <div className="paralelogramo5"></div>
+                <div className="paralelogramo6"></div>
+            </div>
         </div>
     );
 }
