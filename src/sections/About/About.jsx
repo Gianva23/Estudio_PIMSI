2d8aadb7e88b4af0a163a9a247db5fb84ba705a6
Gian <gianlucavanella99@gmail.com>
2025-08-12 14:21:27 -0300

    Contact 1080

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -45,11 +45,6 @@ function About() {
                     </div>
                 </div>
             </div>
-            <div className='detalle-1080'>
-                <div className="paralelogramo4"></div>
-                <div className="paralelogramo5"></div>
-                <div className="paralelogramo6"></div>
-            </div>
         </div>
     );
 }
--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -3,6 +3,11 @@ import './Contact.css'
 function Contact() {
     return (
         <div className='contact-container'>
+            <div className='detalle-1080'>
+                <div className="paralelogramo4"></div>
+                <div className="paralelogramo5"></div>
+                <div className="paralelogramo6"></div>
+            </div>
             <div className='img-back'></div>
             <div className='img-filtro-contact'></div>
             <div className='contact-container-title'>Solicitá una cotización</div>
