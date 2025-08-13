0a54ac280bb4cebf06a28e0471e2595f0725f543
Gian <gianlucavanella99@gmail.com>
2025-08-13 13:43:42 -0300

    Servicios 1080

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import './Card.css';
 import { useState } from 'react';
+import { ReactComponent as Flipicon } from '../../assets/images/icons_flip.svg';
 
 function Card({ frontContent, backContent }) {
     const [isFlipped, setIsFlipped] = useState(false);
@@ -7,6 +8,7 @@ function Card({ frontContent, backContent }) {
     return (
         <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={() => setIsFlipped(!isFlipped)}>
             <div className="flip-card-inner">
+                <div className='container-flip-icon'><Flipicon className='flip-icon'/></div>
                 <div className="flip-card-front">
                     {frontContent}
                 </div>
